089158446aee0930a1a2d203384641a34547cd6b
Andres Juarez <213364@ids.upchiapas.edu.mx>
2024-11-27 17:07:50 -0600

    responsive register and login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,64 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const navigate = useNavigate();
 
   const handleRegister = () => {
-    navigate('/');
+    navigate("/");
   };
 
   const handleDashboard = () => {
-    navigate('/Dashboard');
+    navigate("/Dashboard");
   };
 
   return (
     <>
-      <div className='grid grid-cols-2 font-poppins'>
-        <div className='flex flex-col justify-center items-center'>
-
+      <div className=" lg:grid grid-cols-2 font-poppins">
+        <div className="h-screen flex flex-col justify-center items-center">
           <div className="pb-5 flex flex-col justify-center items-center">
-            <p className='font-semibold text-5xl pb-3'>INICIA SESIÓN</p>
-            <p className='text-base font-light pt-3 w-[75%] text-center'>Rellena los campos con la información correspondiente</p>
+            <div className="lg:hidden flex justify-center mt-4">
+              <img src="/ISSTECH-logo-black.png" alt="" className="h-[70px]" />
+            </div>
+
+            <p className="font-semibold text-3xl mt-5 lg:mt-0 lg:text-5xl">
+              INICIA SESIÓN
+            </p>
+
+            <p className="text-sm lg:text-base font-light pt-3 w-[75%] text-center">
+              Rellena los campos con la información correspondiente
+            </p>
           </div>
 
-          <form className='flex flex-col pt-6 gap-5'>
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14  w-[500px] mb-2' type='email' placeholder='Correo' />
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14' type='password' placeholder='Contraseña' />
-            <button onClick={handleDashboard} className='bg-[#2c2c2c] font-medium text-white rounded-2xl p-4 mt-8'>Inicia sesión</button>
+          <form className="flex flex-col pt-6 gap-5">
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 lg:h-14 w-[300px] lg:w-[500px] mb-2"
+              type="email"
+              placeholder="Correo"
+            />
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 lg:h-14"
+              type="password"
+              placeholder="Contraseña"
+            />
+            <button
+              onClick={handleDashboard}
+              className="bg-[#2c2c2c] font-medium text-white rounded-2xl p-4 mt-8"
+            >
+              Inicia sesión
+            </button>
           </form>
+          <p className="font-light">
+            ¿No tienes una cuenta?{" "}
+            <button
+              onClick={handleRegister}
+              className="bg-white font-medium text-[#2c2c2c] px-0 mt-2 lg:px-10 rounded-2xl py-3 lg:mt-4 mb-2"
+            >
+              Registrate
+            </button>
+          </p>
         </div>
 
-        <div className="relative">
+        <div className="hidden lg:block lg:relative">
           <img
             src="/login-img.png"
             className="w-full h-screen object-cover"
@@ -36,20 +66,28 @@ function Login() {
           />
 
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-80 p-10 w-[470px] rounded-xl shadow-lg text-center text-white">
-            <div className='flex justify-center mt-2'>
-              <img src="/ISSTECH-logo.png" alt="" className='mb-6 h-[70px]' />
+            <div className="flex justify-center mt-2">
+              <img src="/ISSTECH-logo.png" alt="" className="mb-6 h-[70px]" />
             </div>
             <p className="text-4xl font-medium mb-6">BIENVENIDO</p>
-            <p className='text-lg font-light mb-8'>Ingresa tu información para poder acceder a tu cuenta correctamente</p>
-            <div className='border-t-2 mb-6 border-white'></div>
-            <p className='font-light'>¿No tienes una cuenta?</p>
+            <p className="text-lg font-light mb-8">
+              Ingresa tu información para poder acceder a tu cuenta
+              correctamente
+            </p>
+            <div className="border-t-2 mb-6 border-white"></div>
+            <p className="font-light">¿No tienes una cuenta?</p>
 
-            <button onClick={handleRegister} className='bg-white font-medium text-[#2c2c2c] px-10 rounded-2xl py-3 mt-4 mb-2'>Registrate</button>
+            <button
+              onClick={handleRegister}
+              className="bg-white font-medium text-[#2c2c2c] px-10 rounded-2xl py-3 mt-4 mb-2"
+            >
+              Registrate
+            </button>
           </div>
         </div>
-      </div >
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const navigate = useNavigate();
 
   const handleLog = () => {
-    navigate('/Login');
+    navigate("/Login");
   };
 
   return (
     <>
-      <div className='grid grid-cols-2 font-poppins'>
-        <div className='flex flex-col justify-center items-center'>
-          <p className='font-semibold text-5xl'>CREA TU CUENTA</p>
+      <div className="lg:grid grid-cols-2 font-poppins">
+        <div className="h-screen flex flex-col justify-center items-center">
+          <div className="lg:hidden flex justify-center mt-4">
+            <img src="/ISSTECH-logo-black.png" alt="" className="h-[70px]" />
+          </div>
 
-          <p className='text-base font-light pt-3 w-[40%] text-center'>Rellena los campos con la información correspondiente</p>
+          <p className="font-semibold text-3xl mt-5 lg:mt-0 lg:text-5xl">
+            CREA TU CUENTA
+          </p>
 
-          <form className='flex flex-col pt-6 gap-5'>
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14 w-[500px]' type='text' placeholder='Nombre' />
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14' type='text' placeholder='Apellido' />
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14' type='email' placeholder='Correo' />
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14' type='text' placeholder='Firma electrónica' />
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14' type='number' placeholder='Teléfono' />
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14' type='password' placeholder='Contraseña' />
-            <input className='border-[#8d8d8d] border-2 rounded-2xl p-4 h-14' type='password' placeholder='Repite tu contraseña' />
+          <p className="w-[90%] text-sm font-light pt-3 lg:w-[40%] lg:text-base text-center">
+            Rellena los campos con la información correspondiente
+          </p>
 
-            <button onClick={handleLog} className='bg-[#2c2c2c] font-medium text-white rounded-2xl p-4 mt-8'>Registrarse</button>
+          <form className="flex flex-col pt-4 lg:pt-6 gap-5">
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 w-[300px] lg:h-14 lg:w-[500px]"
+              type="text"
+              placeholder="Nombre"
+            />
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 lg:h-14"
+              type="text"
+              placeholder="Apellido"
+            />
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 lg:h-14"
+              type="email"
+              placeholder="Correo"
+            />
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 lg:h-14"
+              type="text"
+              placeholder="Firma electrónica"
+            />
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 lg:h-14"
+              type="number"
+              placeholder="Teléfono"
+            />
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 lg:h-14"
+              type="password"
+              placeholder="Contraseña"
+            />
+            <input
+              className="border-[#8d8d8d] border-2 rounded-2xl p-4 h-12 lg:h-14"
+              type="password"
+              placeholder="Repite tu contraseña"
+            />
+
+            <button
+              onClick={handleLog}
+              className="bg-[#2c2c2c] font-medium text-white rounded-2xl p-4 mt-5 lg:mt-8"
+            >
+              Registrarse
+            </button>
           </form>
+
+          <p className="lg:hidden font-light">
+            ¿Ya tienes una cuenta?{" "}
+            <button
+              onClick={handleLog}
+              className="bg-white font-medium text-[#2c2c2c] mt-2 mb-0 lg:px-10 rounded-2xl py-3 lg:mt-4 lg:mb-2"
+            >
+              Inicia sesión
+            </button>
+          </p>
+
         </div>
 
-        <div className="relative">
+        <div className="hidden lg:block lg:relative">
           <img
             src="/login-img.png"
             className="w-full h-screen object-cover"
@@ -37,19 +89,26 @@ function Login() {
           />
 
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-80 p-10 w-[470px] rounded-xl shadow-lg text-center text-white">
-            <div className='flex justify-center mt-2'>
-              <img src="/ISSTECH-logo.png" alt="" className='mb-6 h-[70px]' />
+            <div className="flex justify-center mt-2">
+              <img src="/ISSTECH-logo.png" alt="" className="mb-6 h-[70px]" />
             </div>
             <p className="text-4xl font-medium mb-6">BIENVENIDO</p>
-            <p className='text-lg font-light mb-8'>Ingresa tu información	para poder crear tu cuenta correctamente</p>
-            <div className='border-t-2 mb-6 border-white'></div>
-            <p className='font-light'>¿Ya tienes una cuenta?</p>
-            <button onClick={handleLog} className='bg-white font-medium text-[#2c2c2c] px-10 rounded-2xl py-3 mt-4 mb-2'>Inicia sesión</button>
+            <p className="text-lg font-light mb-8">
+              Ingresa tu información para poder crear tu cuenta correctamente
+            </p>
+            <div className="border-t-2 mb-6 border-white"></div>
+            <p className="font-light">¿Ya tienes una cuenta?</p>
+            <button
+              onClick={handleLog}
+              className="bg-white font-medium text-[#2c2c2c] px-10 rounded-2xl py-3 mt-4 mb-2"
+            >
+              Inicia sesión
+            </button>
           </div>
         </div>
-      </div >
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
